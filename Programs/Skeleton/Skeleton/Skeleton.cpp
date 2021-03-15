ab5d8f3366321617ff5c2ee05b3aa333be89eff8
Demeter Zalán <zalandemeter12@gmail.com>
2021-03-15 16:57:30 +0100

    pan implemented

--- Programs/Skeleton/Skeleton/Skeleton.cpp
+++ Programs/Skeleton/Skeleton/Skeleton.cpp
@@ -64,31 +64,26 @@ const char * const fragmentSource = R"(
 GPUProgram gpuProgram;	// vertex and fragment shaders
 
 class Graph {
+	unsigned int vao0, vao1;
+public:
 	static const int NUM_OF_VERTICES = 50;
 	static const int NUM_OF_CONNECTIONS = 61;
 
-	unsigned int vao0;
-	unsigned int vao1;
-	float sx, sy;		// scaling
-	vec2 wTranslate;	// translation
-	float phi;			// angle of rotation
-public:
-	vec4 vertices[NUM_OF_VERTICES];
+	vec3 vertices[NUM_OF_VERTICES];
 	vec2 connections[NUM_OF_CONNECTIONS];
+	float arrayV[NUM_OF_VERTICES * 2];
+	float arrayC[NUM_OF_CONNECTIONS * 4];
 
 	Graph() {
 		for (int i = 0; i < NUM_OF_VERTICES; ++i) {
-			//Random number generation from stackoverflow
-			//https://stackoverflow.com/questions/686353/random-float-number-generation
-			float MAX = 3.5;
-			float MIN = -3.5;
+			//Random number generation from stackoverflow https://stackoverflow.com/questions/686353/random-float-number-generation
+			float MAX = 1, MIN = -MAX; 
 			float x = MIN + (float)(rand()) / ((float)(RAND_MAX / (MAX - MIN)));
 			float y = MIN + (float)(rand()) / ((float)(RAND_MAX / (MAX - MIN)));
-			vertices[i] = vec4(x, y, 0, sqrt(x * x + y * y + 1));
+			vertices[i] = vec3(x, y, sqrt(x * x + y * y + 1));
 		}
-
-		int i = 0;
-		int idx = 0;
+		
+		int i = 0; int idx = 0;
 		while (i < NUM_OF_CONNECTIONS) {
 			int idx1 = rand() % NUM_OF_VERTICES, idx2 = rand() % NUM_OF_VERTICES;
 			if (idx1 == idx2) continue;
@@ -104,83 +99,67 @@ public:
 			++i;
 		}
 
-		sx = 1; sy = 1;
-		wTranslate = vec2(0, 0);
-		phi = 0;
+		beltramiKlein();
 	}
 
-	void Create() {
+	void beltramiKlein() {
+		int idxV = 0;
+		for (int i = 0; i < NUM_OF_VERTICES; ++i) {
+			arrayV[idxV++] = vertices[i].x / vertices[i].z;
+			arrayV[idxV++] = vertices[i].y / vertices[i].z;
+		}
+		int idxC = 0;
+		for (int i = 0; i < NUM_OF_CONNECTIONS; ++i) {
+			arrayC[idxC++] = vertices[(const int)connections[i].x].x / vertices[(const int)connections[i].x].z;
+			arrayC[idxC++] = vertices[(const int)connections[i].x].y / vertices[(const int)connections[i].x].z;
+			arrayC[idxC++] = vertices[(const int)connections[i].y].x / vertices[(const int)connections[i].y].z;
+			arrayC[idxC++] = vertices[(const int)connections[i].y].y / vertices[(const int)connections[i].y].z;
+		}
+	}
+
+	void create() {
 		glGenVertexArrays(1, &vao0);
 		glBindVertexArray(vao0);
 		unsigned int vbo0;
 		glGenBuffers(1, &vbo0);
 		glBindBuffer(GL_ARRAY_BUFFER, vbo0);
-		int idxV = 0;
-		float arrayV[NUM_OF_VERTICES * 2];
-		for (int i = 0; i < NUM_OF_VERTICES; ++i) {
-			arrayV[idxV++] = vertices[i].x / vertices[i].w;
-			arrayV[idxV++] = vertices[i].y / vertices[i].w;
-		}
-		glBufferData(GL_ARRAY_BUFFER, sizeof(arrayV), arrayV, GL_DYNAMIC_DRAW);
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, NULL);
 
 		glGenVertexArrays(1, &vao1);
 		glBindVertexArray(vao1);
 		unsigned int vbo1;
 		glGenBuffers(1, &vbo1);
 		glBindBuffer(GL_ARRAY_BUFFER, vbo1);
-		int idxC = 0;
-		float arrayC[NUM_OF_CONNECTIONS * 4];
-		for (int i = 0; i < NUM_OF_CONNECTIONS; ++i) {
-			arrayC[idxC++] = vertices[(const int)connections[i].x].x / vertices[(const int)connections[i].x].w;
-			arrayC[idxC++] = vertices[(const int)connections[i].x].y / vertices[(const int)connections[i].x].w;
-			arrayC[idxC++] = vertices[(const int)connections[i].y].x / vertices[(const int)connections[i].y].w;
-			arrayC[idxC++] = vertices[(const int)connections[i].y].y / vertices[(const int)connections[i].y].w;
-		}
-		glBufferData(GL_ARRAY_BUFFER, sizeof(arrayC), arrayC, GL_DYNAMIC_DRAW);
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, NULL);
 	}
 
-	mat4 M() {
-		mat4 Mscale(sx, 0, 0, 0,
-			0, sy, 0, 0,
-			0, 0, 0, 0,
-			0, 0, 0, 1); // scaling
-
-		mat4 Mrotate(cosf(phi), sinf(phi), 0, 0,
-			-sinf(phi), cosf(phi), 0, 0,
-			0, 0, 1, 0,
-			0, 0, 0, 1); // rotation
-
-		mat4 Mtranslate(1, 0, 0, 0,
-			0, 1, 0, 0,
-			0, 0, 0, 0,
-			wTranslate.x, wTranslate.y, 0, 1); // translation
-
-		return Mscale * Mrotate * Mtranslate;	// model transformation
-	}
-
-	void Draw() {
+	void draw() {
 		glClearColor(0, 0, 0, 0);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		mat4 MVPTransform = M();
+		mat4 MVPTransform = { 1, 0, 0, 0,    // MVP matrix, 
+							  0, 1, 0, 0,    // row-major!
+							  0, 0, 1, 0,
+							  0, 0, 0, 1 };
+
 		gpuProgram.setUniform(MVPTransform, "MVP");
 		int location = glGetUniformLocation(gpuProgram.getId(), "color");
-
+		
 		glBindVertexArray(vao0);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(arrayV), arrayV, GL_DYNAMIC_DRAW);
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, NULL);
 		glUniform3f(location, 1.0f, 0.0f, 0.0f);
 		glDrawArrays(GL_POINTS, 0, NUM_OF_VERTICES);
 
 		glBindVertexArray(vao1);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(arrayC), arrayC, GL_DYNAMIC_DRAW);
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, NULL);
 		glUniform3f(location, 1.0f, 1.0f, 0.0f);
 		glDrawArrays(GL_LINES, 0, NUM_OF_CONNECTIONS * 2);
 		glutSwapBuffers();
 	}
 
-	bool isConnected(const vec4 a, const vec4 b) {
+	bool isConnected(const vec3 a, const vec3 b) {
 		bool connected = false;
 		for (int l = 0; l < NUM_OF_CONNECTIONS; ++l) {
 			const int idx1 = (const int)connections[l].x;
@@ -193,7 +172,7 @@ public:
 		}
 		return connected;
 	}
-	int numOfConnections(vec4 a) {
+	int numOfConnections(vec3 a) {
 		int c = 0;
 		for (int i = 0; i < NUM_OF_CONNECTIONS; ++i)
 			if (vertices[(const int)connections[i].x].x == a.x || vertices[(const int)connections[i].y].x == a.x)
@@ -221,24 +200,41 @@ public:
 	void kMeansClustering() {
 		int* sorted = bubbleSortIdx();
 		for (int i = 0; i < 50; ++i) {
-			int M = 0;
-			vec4 R = vec4(0, 0, 0, 0);
+			int M = 0; vec3 R(0,0,0);
 			for (int j = 0; j < 50; ++j) {
 				if (sorted[i] == j) continue;
 				if (isConnected(vertices[sorted[i]], vertices[j])) {
-					R += (+1) * vertices[j];
+					R = R + (+1) * vertices[j];
 					M++;
 				}
 				else {
-					R += (-1) * vertices[j];
+					R = R + (-1) * vertices[j];
 					M--;
 				}
 			}
 			R = R / M;
-			R.w = sqrt(R.x * R.x + R.y * R.y + 1.0);
 			vertices[sorted[i]] = R;
 		}
 	}
+
+	vec3 beltramiKleinInv(vec2 a) {
+		try {
+			float winv = sqrt(1 - a.x * a.x - a.y * a.y);
+			return vec3(a.x / winv, a.y / winv, 1 / winv);
+		}
+		catch (const std::exception&) {
+			printf("Noninvertible Exception");
+		}
+	}
+
+	float lorentzProduct(vec3 a, vec3 b) {
+		return a.x * b.x + a.y * b.y - a.z * b.z;
+	}
+
+	float hyperbolicDistance(vec3 a, vec3 b) {
+		return acosh(-lorentzProduct(a,b));
+	}
+
 };
 
 Graph graph;
@@ -246,21 +242,21 @@ Graph graph;
 void onInitialization() {
 	glViewport(0, 0, windowWidth, windowHeight);
 	glPointSize(8); glLineWidth(1);
-	graph.Create();
+	graph.create();
 	gpuProgram.create(vertexSource, fragmentSource, "outColor");
 }
 
 void onDisplay() {
-	graph.Draw();
+	graph.draw();
 }
+
+bool startClustering = false;
 
 // Key of ASCII code pressed
 void onKeyboard(unsigned char key, int pX, int pY) {
 	if (key == 'd') glutPostRedisplay();         // if d, invalidate display, i.e. redraw
 	if (key == ' ') {
-		graph.kMeansClustering();
-		graph.Create();
-		graph.Draw();
+		startClustering = true;
 	}
 }
 
@@ -268,12 +264,36 @@ void onKeyboard(unsigned char key, int pX, int pY) {
 void onKeyboardUp(unsigned char key, int pX, int pY) {
 }
 
+vec2 startPoint(0,0);
+vec2 endPoint(0,0);
+
 // Move mouse with key pressed
 void onMouseMotion(int pX, int pY) {	// pX, pY are the pixel coordinates of the cursor in the coordinate system of the operation system
 	// Convert to normalized device space
 	float cX = 2.0f * pX / windowWidth - 1;	// flip y axis
 	float cY = 1.0f - 2.0f * pY / windowHeight;
-	printf("Mouse moved to (%3.2f, %3.2f)\n", cX, cY);
+	
+	startPoint = endPoint;
+	endPoint = vec2(cX, cY);
+	float tmpX = abs((endPoint - startPoint).x) <= FLT_MIN ? 0.0002 : (endPoint - startPoint).x;
+	float tmpY = abs((endPoint - startPoint).y) <= FLT_MIN ? 0.0002 : (endPoint - startPoint).y;
+	vec2 shiftPoint2D = vec2(tmpX, tmpY);
+	vec3 shiftPointHyperbolic = graph.beltramiKleinInv(shiftPoint2D);
+	vec3 hyperbolaBottom = vec3(0, 0, 1);
+	float shiftVectorDistance = graph.hyperbolicDistance(shiftPointHyperbolic, hyperbolaBottom);
+	vec3 shiftVectorVelocity = (shiftPointHyperbolic - hyperbolaBottom * cosh(shiftVectorDistance)) / sinh(shiftVectorDistance);
+	vec3 mirrorPoint1 = hyperbolaBottom * cosh(shiftVectorDistance * 0.25) + shiftVectorVelocity * sinh(shiftVectorDistance * 0.25);
+	vec3 mirrorPoint2 = hyperbolaBottom * cosh(shiftVectorDistance * 0.75) + shiftVectorVelocity * sinh(shiftVectorDistance * 0.75);
+	for (int i = 0; i < graph.NUM_OF_VERTICES; ++i) {
+		float mp1Distance = graph.hyperbolicDistance(mirrorPoint1, graph.vertices[i]);
+		vec3 mp1Velocity = (mirrorPoint1 - graph.vertices[i] * cosh(mp1Distance)) / sinh(mp1Distance);
+		vec3 vertexMirrored1 = graph.vertices[i] * cosh(2 * mp1Distance) + mp1Velocity * sinh(2 * mp1Distance);
+		float mp2Distance = graph.hyperbolicDistance(mirrorPoint2, vertexMirrored1);
+		vec3 mp2Velocity = (mirrorPoint2 - vertexMirrored1 * cosh(mp2Distance)) / sinh(mp2Distance);
+		vec3 vertexMirrored2 = vertexMirrored1 * cosh(2 * mp2Distance) + mp2Velocity * sinh(2 * mp2Distance);
+		graph.vertices[i] = vertexMirrored2;
+	}
+	graph.beltramiKlein();
 }
 
 // Mouse click event
@@ -282,20 +302,18 @@ void onMouse(int button, int state, int pX, int pY) { // pX, pY are the pixel co
 	float cX = 2.0f * pX / windowWidth - 1;	// flip y axis
 	float cY = 1.0f - 2.0f * pY / windowHeight;
 
-	char * buttonStat;
-	switch (state) {
-	case GLUT_DOWN: buttonStat = "pressed"; break;
-	case GLUT_UP:   buttonStat = "released"; break;
-	}
-
-	switch (button) {
-	case GLUT_LEFT_BUTTON:   printf("Left button %s at (%3.2f, %3.2f)\n", buttonStat, cX, cY);   break;
-	case GLUT_MIDDLE_BUTTON: printf("Middle button %s at (%3.2f, %3.2f)\n", buttonStat, cX, cY); break;
-	case GLUT_RIGHT_BUTTON:  printf("Right button %s at (%3.2f, %3.2f)\n", buttonStat, cX, cY);  break;
+	if (button == GLUT_LEFT_BUTTON) {
+		if (state == GLUT_DOWN) { startPoint = vec2(cX, cY); endPoint = vec2(cX, cY); }
 	}
 }
 
 // Idle event indicating that some time elapsed: do animation here
 void onIdle() {
 	long time = glutGet(GLUT_ELAPSED_TIME); // elapsed time since the start of the program
+	if (startClustering) {
+		graph.kMeansClustering();
+		graph.beltramiKlein();
+		startClustering = false;
+	}
+	graph.draw();
 }
