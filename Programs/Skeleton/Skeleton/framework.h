44b38bdcb4d3a448b1e59937a9a24b287ce7cf92
Demeter Zalán <zalandemeter12@gmail.com>
2021-03-23 21:48:40 +0100

    v1.2

--- Programs/Skeleton/Skeleton/framework.h
+++ Programs/Skeleton/Skeleton/framework.h
@@ -24,7 +24,7 @@
 #endif
 
 // Resolution of screen
-const unsigned int windowWidth = 800, windowHeight = 800;
+const unsigned int windowWidth = 600, windowHeight = 600;
 
 //--------------------------
 struct vec2 {
